6052e59f015fad4f07fc9c640def928ccba01095
Arthur Lopes <arthurlopr12@gmail.com>
2024-07-10 20:52:39 -0300

    feat: create service to send notification

---
+++ src/main/java/arthurlopes/admincatalog/admincatalog/AdminCatalogApplication.java
@@ -0,0 +1,13 @@
+package arthurlopes.admincatalog.admincatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/arthurlopes/admincatalog/admincatalog/AdminCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package arthurlopes.admincatalog.admincatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
